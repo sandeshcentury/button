490dc8261d29bd2c303760f50b40bd13a149b301
Sandesh Century <ronaldosandesh07@gmail.com>
2023-06-30 17:06:33 +0545

    button

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let name_index = parseInt(title.getAttribute('data-index'))
 console.log(names[name_index])
 
     if(name_index == names.length -1){
-      title.setAttribute('data-index', name_index +1)
-      title.innerHTML = names[name_index +1]
+      title.setAttribute('data-index', 0)
+      title.innerHTML = names[0]
 
     } else{
       title.setAttribute('data-index', name_index +1)
@@ -20,3 +20,22 @@ console.log(names[name_index])
 }
 
 btn.addEventListener("click", clickMe)
+
+
+let button = ["Messi","Mbappe","Haaland","Ronaldo"]
+
+function click(){
+  let name_index = parseInt(btn.getAttribute('data-index'))
+console.log(button[name_index])
+
+    if(name_index == button.length -1){
+      btn.setAttribute('data-index', 0)
+      btn.innerHTML = button[0]
+
+    } else{
+      btn.setAttribute('data-index', name_index +1)
+      btn.innerHTML = button[name_index +1]
+    }
+}
+
+btn.addEventListener("click", click)
